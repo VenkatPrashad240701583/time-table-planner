e497303d194921fafed5849603b5671ef31052e3
VenkatPrashad240701583 <venkatprashad.a.2024.cse@rajalakshmi.edu.in>
2026-05-15 00:49:52 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",   # path to your FastAPI app
         host="0.0.0.0",
-        port=80,
+        port=8000,
         reload=True       # auto-restart on changes
     )
